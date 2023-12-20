345d4d941d6525790d08cd72abab11ebb7ff7de0
Nicholas Sandomeno <nsando61@gmail.com>
2023-12-20 17:16:53 -0500

    clean

--- src/config.rs
+++ src/config.rs
@@ -11,11 +11,6 @@ pub struct Config {
     pub api_client_id: String,
     pub api_client_secret: String,
     pub node_id: String,
-    //pub username: String,
-    //pub user_id: String,
-    //pub uma_encryption_private_key_hex: String,
-    //pub uma_signing_private_key_hex: String,
-    // pub node_master_seed_hex: String,
     pub client_base_url: String,
     pub api_port: u16,
 }
@@ -29,27 +24,13 @@ impl Config {
         let api_port = std::env::var("LIGHTSPARK_API_PORT").ok();
         let api_client_id = std::env::var("LIGHTSPARK_API_CLIENT_ID").ok();
         let api_client_secret = std::env::var("LIGHTSPARK_API_CLIENT_SECRET").ok();
-        //let master_seed_hex = std::env::var("LIGHTSPARK_MASTER_SEED_HEX").ok();
         let node_id = std::env::var("LIGHTSPARK_NODE_ID").ok();
-        //let username = std::env::var("LIGHTSPARK_USERNAME").ok();
-        //let user_id = std::env::var("LIGHTSPARK_USER_ID").ok();
-        // let uma_encryption_private_key_hex =
-        //     std::env::var("LIGHTSPARK_UMA_ENCRYPTION_PRIVATE_KEY_HEX").ok();
-        // let uma_signing_private_key_hex =
-        //     std::env::var("LIGHTSPARK_UMA_SIGNING_PRIVATE_KEY_HEX").ok();
 
         Config {
-            // TODO consider use of unwrap_or_default, doesn't look like their are defaults...
-
             log_level: log_level.unwrap_or_default(),
             api_client_id: api_client_id.unwrap_or_default(),
             api_client_secret: api_client_secret.unwrap_or_default(),
             node_id: node_id.unwrap_or_default(),
-            //username: username.unwrap_or_default(),
-            //user_id: user_id.unwrap_or_default(),
-            //uma_encryption_private_key_hex: uma_encryption_private_key_hex.unwrap_or_default(),
-            //uma_signing_private_key_hex: uma_signing_private_key_hex.unwrap_or_default(),
-            //node_master_seed_hex: master_seed_hex.unwrap_or_default(),
             client_base_url: api_endpoint.unwrap_or_default(),
             api_port: api_port
                 .unwrap_or("8080".to_string())
